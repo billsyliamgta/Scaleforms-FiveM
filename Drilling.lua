2aa7d3335d9db0d735033c7faaae884157912737
Liam <160941195+billsyliamgta@users.noreply.github.com>
2025-05-14 08:55:34 +0100

    Create Drilling.lua

---
+++ Drilling.lua
@@ -0,0 +1,25 @@
+local handle
+
+function LoadScaleform()
+    print("CLIENT: Requesting Drilling Scaleform..")
+    local start = GetGameTimer()
+    local time = 5000
+    handle = RequestScaleformMovie("DRILLING")
+    while not HasScaleformMovieLoaded(handle) do
+        if GetGameTimer() > start + time then
+            print("CLIENT: Timed out while loading Scaleform Movie 'DRILLING'.")
+        end
+        Citizen.Wait(0)
+    end
+    print("CLIENT: Loaded Drilling Scaleform.")
+end
+end)
+
+function DeleteScaleform()
+    if HasScaleformMovieLoaded(handle) then
+        SetScaleformMovieAsNoLongerNeeded(handle)
+        handle = 0
+    end
+
+    print("CLIENT: Cleaned up Drilling Scaleform.")
+end
